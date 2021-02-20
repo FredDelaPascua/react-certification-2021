4dc7c31481fb267e1bb175082f1f90dc2bc418e4
Fred De la Pascua <federico.gutierrez@federicogutierrez.local>
2021-02-20 00:57:24 -0500

    Updates

---
+++ precache-manifest.8a45dc098b06ba5dadf2062974e595a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c312bb541183551acac",
+    "url": "/react-certification-2021/static/css/main.77c36dd3.chunk.css"
+  },
+  {
+    "revision": "0c312bb541183551acac",
+    "url": "/react-certification-2021/static/js/main.562cbad2.chunk.js"
+  },
+  {
+    "revision": "15d914c744b113deb9b8",
+    "url": "/react-certification-2021/static/js/runtime~main.1611d612.js"
+  },
+  {
+    "revision": "3704f5a3e750f1adbfbe",
+    "url": "/react-certification-2021/static/js/2.3eb20625.chunk.js"
+  },
+  {
+    "revision": "9d2118af6158cc718cda3acc604262d0",
+    "url": "/react-certification-2021/index.html"
+  }
+];
